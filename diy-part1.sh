aba76c46833eba643fdecef4ab42c5aa5567c731
Wei Liu <love_liuweiwei@163.com>
2022-03-22 21:26:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,12 +11,12 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git passwallluci https://github.com/xiaorouji/openwrt-passwall/tree/luci' feeds.conf.default
-sed -i '$a src-git serverchan https://github.com/tty228/luci-app-serverchan' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git passwallluci https://github.com/xiaorouji/openwrt-passwall/tree/luci' feeds.conf.default
+# sed -i '$a src-git serverchan https://github.com/tty228/luci-app-serverchan' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
