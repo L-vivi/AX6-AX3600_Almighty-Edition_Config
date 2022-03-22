b5a2c81e34ede550f542dc1f9aa57ac917a2810f
Wei Liu <love_liuweiwei@163.com>
2022-03-22 18:20:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,5 @@ sed -i '$a src-git passwallluci https://github.com/xiaorouji/openwrt-passwall/tr
 sed -i '$a src-git serverchan https://github.com/tty228/luci-app-serverchan' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
